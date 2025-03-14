d6764926d2919866411579fc28de582bd7bd526a
Johannes Breitfeld <jbvbreitfeld@gmail.com>
2025-03-14 15:14:53 +0100

    Create entities (#1)
    * Create entities Order.cs, Product.cs and OrderProduct.cs. 
      Create OrderProductConfiguration.cs. Update AppContext.cs
    
    * Create entity Customer.cs and update Order.cs.
      Create CustomerConfiguration.cs

---
+++ Fullstack.Domain/Entities/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace Fullstack.Domain.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+        public DateTime DateTime { get; set; }
+        public ICollection<OrderProduct> OrderProducts { get; set; } = new List<OrderProduct>();
+    }
+}
--- Fullstack.Persistance/Data/AppContext.cs
+++ Fullstack.Persistance/Data/AppContext.cs
@@ -1,8 +1,24 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Fullstack.Domain.Entities;
+using Fullstack.Persistance.Data.Configurations;
+using Microsoft.EntityFrameworkCore;
 
-namespace Fullstack.Persistance.Data
+namespace Fullstack.Persistance.Data;
+
+public class AppContext : DbContext
 {
-    public class AppContext : DbContext
+    public AppContext(DbContextOptions<AppContext> options) : base(options) { }
+
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderProduct> OrderProducts { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.ApplyConfiguration(new OrderProductConfiguration());
+        modelBuilder.ApplyConfiguration(new CustomerConfiguration());
+
+        base.OnModelCreating(modelBuilder);
+      
     }
 }

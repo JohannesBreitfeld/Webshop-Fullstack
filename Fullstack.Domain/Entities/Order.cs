1cf2c4d7992228dc050fbb44abd62f6643a60b48
Johannes Breitfeld <jbvbreitfeld@gmail.com>
2025-03-14 11:57:02 +0100

    Create entities Order.cs, Product.cs and OrderProduct.cs. Create OrderProductConfiguration.cs. Update AppContext.cs

---
+++ Fullstack.Domain/Entities/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace Fullstack.Domain.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public DateTime DateTime { get; set; }
+        public ICollection<OrderProduct> OrderProducts { get; set; } = new List<OrderProduct>()
+    }
+}
--- Fullstack.Persistance/Data/AppContext.cs
+++ Fullstack.Persistance/Data/AppContext.cs
@@ -1,8 +1,21 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Fullstack.Domain.Entities;
+using Fullstack.Persistance.Data.Configurations;
+using Microsoft.EntityFrameworkCore;
 
-namespace Fullstack.Persistance.Data
+namespace Fullstack.Persistance.Data;
+
+public class AppContext : DbContext
 {
-    public class AppContext : DbContext
+    public AppContext(DbContextOptions<AppContext> options) : base(options) { }
+
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderProduct> OrderProducts { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.ApplyConfiguration(new OrderProductConfiguration());
+
+        base.OnModelCreating(modelBuilder);
     }
 }
